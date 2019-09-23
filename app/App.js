5077375a3f35c65101ea02016490efa8619787ea
gonzo <m165252@daum.net>
2019-09-23 21:12:14 +0900

    Icon 요소 추가 하기 #13

--- app/App.js
+++ app/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import {ApplicationProvider} from 'react-native-ui-kitten';
+import {ApplicationProvider, IconRegistry} from 'react-native-ui-kitten';
 import {mapping, light as theme} from '@eva-design/eva';
+import {EvaIconsPack} from '@ui-kitten/eva-icons';
 import Root from "./src/root";
 
 const App = () => (
     <ApplicationProvider mapping={mapping} theme={theme}>
-      <Root/>
+        <IconRegistry icons={EvaIconsPack}/>
+        <Root/>
     </ApplicationProvider>
 );
 
