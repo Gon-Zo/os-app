57a234070b53f1b88a6611ac08fab21fd72af4cd
gonzo <m165252@daum.net>
2019-09-26 21:00:17 +0900

    Home Screen 레이아웃 구성 #16

--- app/src/screen/HomeScreen.js
+++ app/src/screen/HomeScreen.js
@@ -2,14 +2,16 @@ import React, {Component} from 'react';
 import {Layout, Text, Button} from 'react-native-ui-kitten'
 import {SafeAreaView, View, StyleSheet, ScrollView, StatusBar, Image, Dimensions, TextInput,} from 'react-native';
 import ImgBanner from "../component/ImgBanner";
-import CategoryCard from "../component/CategoryCard";
+import CategoryGroup from "../component/CategoryGroup";
 import SearchBox from "../component/SearchBox";
+import TextCard from "../component/textCard";
+import ItemList from "../component/ItemList";
 
 export default class HomeScreen extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state = {nav : this.props.navigation}
+        this.state = {nav: this.props.navigation}
     }
 
     static navigationOptions = {
@@ -28,10 +30,12 @@ export default class HomeScreen extends Component {
             <View style={{width: '100%', height: '100%', backgroundColor: '#f00'}}>
                 <StatusBar barStyle="dark-content"/>
                 <SafeAreaView style={{width: '100%', height: '100%', backgroundColor: '#f0f'}}>
-                    <ScrollView style={{backgroundColor: '#fff'}}>
+                    <ScrollView style={{backgroundColor: '#e5e5e5'}}>
                         <ImgBanner/>
-                        <CategoryCard/>
+                        <CategoryGroup/>
                         <SearchBox/>
+                        <TextCard title={'인기 상품'} />
+                        <ItemList/>
                     </ScrollView>
                 </SafeAreaView>
             </View>
