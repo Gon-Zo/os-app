c832fce75dfec67fbee50ab5ad125445b5ef0000
gonzo <m165252@daum.net>
2019-09-26 20:58:14 +0900

    App Router Setting #5

--- app/App.js
+++ app/App.js
@@ -4,7 +4,16 @@ import {mapping, light as theme} from '@eva-design/eva';
 import {EvaIconsPack} from '@ui-kitten/eva-icons';
 import Root from "./src/root";
 
+
+// import {Provider} from 'react-redux'
+// import {store} from './src/store/index'
+{/*<Provider store={store}>*/
+}
+// </Provider>
+
+
 const App = () => (
+
     <ApplicationProvider mapping={mapping} theme={theme}>
         <IconRegistry icons={EvaIconsPack}/>
         <Root/>
