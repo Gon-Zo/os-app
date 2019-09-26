ace2ea85a5d2f30e66d4d54ba77f368cdcba3cdb
gonzo <m165252@daum.net>
2019-09-26 20:58:40 +0900

    App Router Setting #5

--- app/src/screen/app_router.js
+++ app/src/screen/app_router.js
@@ -3,68 +3,27 @@ import React from 'react-native'
 import {createAppContainer} from 'react-navigation'
 import {createStackNavigator} from 'react-navigation-stack'
 import {createBottomTabNavigator} from 'react-navigation-tabs'
+import {createDrawerNavigator} from "react-navigation-drawer";
 
 // import Screen
 import HomeScreen from "./HomeScreen";
 import ProfileScreen from "./ProfileScreen";
-import {createDrawerNavigator} from "react-navigation-drawer";
 import PopScreen from "./PopScreen";
 import LoadSpinner from "../component/LoadSpinner";
 import Test1 from "./drawer/Test1";
 import Test2 from "./drawer/Test2";
-
-
 import DrawerNavigation from './drawer/index'
 
 
-const homStackNav = createStackNavigator({
-    Home: {
-        screen: HomeScreen,
-        // header style change
-        navigationOptions: {
-            headerStyle: {
-                backgroundColor: '#fff',
-                borderBottomWidth: 5,
-                borderBottomColor: '#000',
-                shadowOpacity: 0,
-                elevation: 0,
-            },
-        },
-    }
-});
-
-const profileStackNav = createStackNavigator({
-    Profile: {screen: ProfileScreen}
-});
-
-const bottomNav = createBottomTabNavigator({
-    Home: homStackNav,
-    Profile: profileStackNav
-});
-
-const popNav = createDrawerNavigator({
-    Pop: PopScreen,
-});
-
-const loadNav = createDrawerNavigator({
-    Load: LoadSpinner
-});
-
-const DrawerNavigator = createDrawerNavigator({
-    Test1: Test1,
-    Test2: Test2
-}, {
-    contentComponent: DrawerNavigation,
+const mainRoot = createBottomTabNavigator({
+    Home: {screen: HomeScreen},
+    Profile: {screen: ProfileScreen},
 });
 
 
-// Public Screens
 const MyApp = createStackNavigator({
-    Bottom: bottomNav,
-    Pop: popNav,
-    Load: loadNav,
-    Menu: DrawerNavigator
-}, {headerMode: "none"});
+    Main: mainRoot
+}, {headerMode : "none"});
 
 const AppRouter = createAppContainer(MyApp);
 
