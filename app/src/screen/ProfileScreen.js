32611c7aa9cb751b8126efa827f40c9d07396b0a
gonzo <m165252@daum.net>
2019-09-30 20:13:30 +0900

    [fix] UpdateScreen Layout 구성 #21

--- app/src/screen/ProfileScreen.js
+++ app/src/screen/ProfileScreen.js
@@ -1,17 +1,20 @@
 import React, {Component} from 'react';
-import {Layout, Text, Button} from 'react-native-ui-kitten'
+import {View, StyleSheet, TextInput} from 'react-native'
+import {Layout, Text, Button, Input} from 'react-native-ui-kitten'
+
 
 export default class ProfileScreen extends Component {
-
-    static navigationOptions = {
-        headerTitle: 'Profile',
-    };
 
     render() {
         return (
             <Layout>
-                <Text>ProfileScreen</Text>
-                <Button onPress={() => this.props.navigation.navigate("Load")}>Button click</Button>
+                <View style={{
+                    backgroundColor: '#e5e5e5',
+                    width: '100%',
+                    height: '100%',
+                }}>
+
+                </View>
             </Layout>
         );
     }
