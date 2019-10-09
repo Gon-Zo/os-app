c80501eeea56126f1b87d50d5d4261b6f7a38561
gonzo <m165252@daum.net>
2019-10-09 16:10:29 +0900

    Login Screen Make #24

--- app/src/containers/root/index.js
+++ app/src/containers/root/index.js
@@ -6,10 +6,12 @@ import {createStackNavigator} from 'react-navigation-stack'
 // main Root
 import MainRoot from "../main";
 import FormNav from "../single/update";
+import LoginNav from "../single/login";
 
 const MyApp = createStackNavigator({
     Main: MainRoot,
     Update: FormNav,
+    Login : LoginNav
 }, {headerMode: "none"});
 
 const AppRouter = createAppContainer(MyApp);
