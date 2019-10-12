40892ab10d04df273256f36a717b86246fa84517
gonzo <m165252@daum.net>
2019-10-12 13:29:54 +0900

    Make to Custom Bottom Tap #34

--- app/src/containers/main/index.js
+++ app/src/containers/main/index.js
@@ -1,12 +1,44 @@
 import {createBottomTabNavigator} from "react-navigation-tabs";
+
 import SettingScreen from "./setting";
 import HomeScreen from "./home";
 import Test2Screen from "./temp";
+
+
+import Icon from 'react-native-vector-icons/FontAwesome';
+import React from "react";
+
 
 const MainRoot = createBottomTabNavigator({
     Temp: {screen: Test2Screen},
     Setting: {screen: SettingScreen},
     Home: {screen: HomeScreen, navigationOptions: {}},
+}, {
+    defaultNavigationOptions: ({navigation}) => ({
+        // custom tab bar
+        tabBarIcon: ({focused, horizontal, tintColor}) => {
+            const {routeName} = navigation.state;
+            return <Icon name={getIcon(routeName)} size={24} color={tintColor}/>
+        },
+    }),
+    tabBarOptions: {
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+    },
 });
+
+function getIcon(tapName) {
+    switch (tapName) {
+        case "Home" :
+            return "home";
+        case "Setting":
+            return "cog";
+        case "Temp":
+            return "inbox";
+        default:
+            return "home";
+    }
+}
+
 
 export default MainRoot
--- app/src/containers/root/index.js
+++ app/src/containers/root/index.js
@@ -7,11 +7,15 @@ import {createStackNavigator} from 'react-navigation-stack'
 import MainRoot from "../main";
 import FormNav from "../single/update";
 import LoginNav from "../single/login";
+import DetailNav from "../single/detail";
+import SignNav from "../single/sign";
 
 const MyApp = createStackNavigator({
     Main: MainRoot,
     Update: FormNav,
-    Login : LoginNav
+    Login: LoginNav,
+    Detail : DetailNav,
+    Sign : SignNav
 }, {headerMode: "none"});
 
 const AppRouter = createAppContainer(MyApp);
