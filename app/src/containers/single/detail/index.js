8779cbea3580f9d0a4584a7f670bd87f373f7236
gonzo <m165252@daum.net>
2019-10-12 13:14:20 +0900

    Detail Screen 그리드 구성 #29

---
+++ app/src/containers/single/detail/index.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    StatusBar,
+} from 'react-native';
+import {createDrawerNavigator} from "react-navigation-drawer";
+import DetailCard from "../../../components/DetailCard";
+import DetailContent from "../../../components/DetailContent";
+import DetailTapScreen from "../../../components/Tap";
+
+class DetailScreen extends Component {
+    render() {
+        return (
+            <View>
+                <StatusBar barStyle="dark-content"/>
+                <SafeAreaView>
+                    <ScrollView style={{backgroundColor : '#c9c9c9'}}>
+                        <DetailCard/>
+                        <DetailContent/>
+                        <DetailTapScreen/>
+                    </ScrollView>
+                </SafeAreaView>
+            </View>
+        );
+    }
+}
+
+const DetailNav = createDrawerNavigator({
+    Detail: DetailScreen
+})
+
+export default DetailNav
