30326f80ef19ea10d6e0fcb14050e4e1823fbbd9
gonzo <m165252@daum.net>
2019-10-12 13:32:06 +0900

    application 안정화 #32

--- app/src/components/MenuGroup.js
+++ app/src/components/MenuGroup.js
@@ -7,27 +7,29 @@ import {
 import {Layout, Text} from "react-native-ui-kitten";
 
 const MenuTemp = [
-    {title:"Menu1"},
-    {title:"Menu2"},
-    {title:"Menu3"},
-    {title:"Menu4"},
-    {title:"Menu5"},
-    {title:"Menu6"},
+    {id: 1, title: "Menu1"},
+    {id: 2, title: "Menu2"},
+    {id: 3, title: "Menu3"},
+    {id: 4, title: "Menu4"},
+    {id: 5, title: "Menu5"},
+    {id: 6, title: "Menu6"},
 ]
 
 class MenuButton extends Component {
     render() {
         return (
-            <TouchableOpacity activeOpacity={1} style={{
+            <TouchableOpacity
+                key={this.props.data.id}
+                activeOpacity={1} style={{
                 width: '100%',
                 height: 50,
                 backgroundColor: '#f00',
                 alignItems: 'center',
                 justifyContent: 'center',
             }} onPress={() => {
-                alert("test success")
+                alert("test success >> " + this.props.data.id)
             }}>
-                <Text>{this.props.text}</Text>
+                <Text>{this.props.data.title}</Text>
             </TouchableOpacity>
         )
     }
@@ -35,16 +37,14 @@ class MenuButton extends Component {
 
 
 export default class MenuGroup extends Component {
-    renderMenuBtn(text) {
-        return <MenuButton text={text}/>
+    renderMenuBtn(obj) {
+        return <MenuButton data={obj}/>
     }
 
     render() {
         return (
             <Layout style={styles.wrap}>
-                {
-                    MenuTemp.map((i) => this.renderMenuBtn(i.title))
-                }
+                {MenuTemp.map((i) => this.renderMenuBtn(i))}
             </Layout>
         );
     }
