3d77a34fed12f77dde4a0f2e02e24616c599e7de
gonzo <m165252@daum.net>
2019-10-12 13:25:14 +0900

    Login Screen Make #24

--- app/src/containers/single/login/index.js
+++ app/src/containers/single/login/index.js
@@ -2,12 +2,14 @@ import React, {Component} from 'react';
 import {Layout} from "react-native-ui-kitten";
 import {createDrawerNavigator} from "react-navigation-drawer";
 import LoginForm from "../../../components/LoginForm";
+import TextButton from "../../../components/TextButton";
 
 class LoginScreen extends Component {
     render() {
         return (
             <Layout>
                 <LoginForm navigation={this.props.navigation}/>
+                <TextButton navigation={this.props.navigation} text={'Test text button'}/>
             </Layout>
         );
     }
