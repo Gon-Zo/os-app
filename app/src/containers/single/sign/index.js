a28def837be84d6f0eb1de998ae82c6cc65a819b
gonzo <m165252@daum.net>
2019-10-12 13:34:09 +0900

    Sign Screen 그리드 구성 #35

---
+++ app/src/containers/single/sign/index.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    StatusBar,
+    TextInput,
+    Button
+} from 'react-native';
+import SignForm from "../../../components/SingForm";
+import {createDrawerNavigator} from "react-navigation-drawer";
+
+class SignScreen extends Component {
+    render() {
+        return (
+            <View>
+                <StatusBar barStyle="dark-content"/>
+                <SafeAreaView>
+                    <ScrollView>
+
+
+                        <SignForm/>
+
+                        {/*<View>*/}
+                        {/*    <Button title={'Sign up'} onPress={()=>{*/}
+                        {/*        alert("sign up success")*/}
+                        {/*    }} />*/}
+                        {/**/}
+                        {/*</View>*/}
+
+
+                    </ScrollView>
+                </SafeAreaView>
+            </View>
+        );
+    }
+}
+
+const SignNav = createDrawerNavigator({
+    Sign: {screen: SignScreen}
+});
+
+export default SignNav
