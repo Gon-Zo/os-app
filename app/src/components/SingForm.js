1aa44780dc122c099cb4df449c5cec89cba21b35
gonzo <m165252@daum.net>
2019-10-29 20:10:43 +0900

    Sign Form 개발 #38

--- app/src/components/SingForm.js
+++ app/src/components/SingForm.js
@@ -7,11 +7,10 @@ import {
     Text,
     StatusBar, TextInput,
 } from 'react-native';
-import ActionCreators from "../actions";
-import {connect} from 'react-redux'
+import InputText from "./update/sign/InputText";
+import {TEXT_INPUT} from "../public/types/State";
 
-
-class SignForm extends Component {
+export default class SignForm extends Component {
 
     constructor(props, context) {
         super(props, context)
@@ -21,99 +20,26 @@ class SignForm extends Component {
         return (
             <View>
 
-                <View style={styles.wrap}>
-                    <TextInput
-                        placeholder={'ID'}
-                        style={styles.input}
-                        onChangeText={(text) => {
-                            this.setState({text});
+                <InputText width={'100%'}
+                           placeholder={'ID'}
+                           keyType={TEXT_INPUT.DEFAULT}/>
 
-                            if (text !== '') {
-                                this.props.putSignId(text)
-                            }
-
-                        }}
+                <View style={{
+                    flexDirection : 'row'
+                }}>
+                    <InputText
+                        width={'50%'}
+                        placeholder={'Number'}
+                        keyType={TEXT_INPUT.NUMERIC}
                     />
-                    <View style={styles.text}>
-                        <Text>Msg</Text>
-                    </View>
+                    <InputText
+                        width={'50%'}
+                        placeholder={'Text'}
+                        keyType={TEXT_INPUT.DEFAULT}
+                    />
                 </View>
-
-                {/*<View style={styles.wrap}>*/}
-                {/*    <TextInput*/}
-                {/*        placeholder={'Password'}*/}
-                {/*        style={styles.input}*/}
-                {/*        onChangeText={(text) => {*/}
-                {/*            this.setState({text});*/}
-                {/*            if (text !== '') {*/}
-
-                {/*                this.props.checkSignId(text)*/}
-
-                {/*            }*/}
-                {/*        }}*/}
-                {/*    />*/}
-                {/*    <View style={styles.text}>*/}
-                {/*        <Text>Msg</Text>*/}
-                {/*    </View>*/}
-                {/*</View>*/}
 
             </View>
         );
     }
 }
-
-function mapStateToProps(state) {
-    return {
-        id: state.sign.singInfo.id,
-        pwd: state.sign.singInfo.pwd,
-        address: state.sign.singInfo.address,
-    }
-}
-
-function mapDispatchToProps(dispatch) {
-    return {
-
-        putSignId: (text) => {
-            dispatch(ActionCreators.checkSignId(text));
-        },
-
-        // putSignPwd: (text) => {
-        //     dispatch(ActionCreators.checkSignPwd(text));
-        // },
-        //
-        // putSignAddress: (text) => {
-        //     dispatch(ActionCreators.checkSignAddress(text));
-        // },
-        //
-
-    }
-}
-
-
-const styles = StyleSheet.create({
-
-    wrap: {
-        width: '100%',
-        height: 80,
-        borderColor: '#f00',
-        borderWidth: 1
-    },
-
-    input: {
-        width: '100%',
-        height: 50,
-        backgroundColor: '#00f',
-        color: "#fff"
-    },
-
-    msg: {
-        width: '100%',
-        height: 30,
-        backgroundColor: '#0f0'
-    }
-
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(SignForm)
-
-// export default SignForm
