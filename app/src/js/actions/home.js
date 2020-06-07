7ffb8e9db44aad1334fae8af1b1fac3bd64157a4
gonzo <m165252@daum.net>
2020-06-07 17:42:37 +0900

    #004 http setting

--- app/src/js/actions/home.js
+++ app/src/js/actions/home.js
@@ -1,10 +1,8 @@
 import {onInput , onOutput , onLoad , onError} from '../reducers/home'
-import axios from 'axios'
 
 export const httpInput = function (dispatch) {
         dispatch(onLoad())
         try {
-                // axios.get(`https://localhost:8080/health/check-up`)
                 dispatch(onInput())
         } catch (e) {
                 dispatch(onError(e))
